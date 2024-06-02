847a38bbb692f0f2de9274290d3126eb2ab7c0cb
Park JunHyoung <pj93251428@gmail.com>
2024-06-02 21:49:51 +0900

    chore

--- Assets/_Scripts/01.DeafaultVisualizer/CubeSpawner.cs
+++ Assets/_Scripts/01.DeafaultVisualizer/CubeSpawner.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace defaultVisualizer
+namespace deafaultVisualizer
 {
     public class CubeSpawner : MonoBehaviour
     {
