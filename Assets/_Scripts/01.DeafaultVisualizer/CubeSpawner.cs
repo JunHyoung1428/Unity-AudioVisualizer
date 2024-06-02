b51f36fd3b7c740a11c3dfe9a836db0745aa500f
Park JunHyoung <pj93251428@gmail.com>
2024-06-02 21:49:23 +0900

    chore

--- Assets/_Scripts/01.DeafaultVisualizer/CubeSpawner.cs
+++ Assets/_Scripts/01.DeafaultVisualizer/CubeSpawner.cs
@@ -2,63 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CubeSpawner : MonoBehaviour
+namespace defaultVisualizer
 {
-    [SerializeField] private GameObject cube;
-    private GameObject[] cubes;
-
-    [Header("Settings")]
-    [SerializeField] float spawnRadius = 10f;
-    [Range(0f,100f)]
-    [SerializeField] float sizePower  =20f;
-
-    [Space(2)]
-    [Header("Scales")]
-    [Range(0,2)]
-    [SerializeField] float minYScale = 1.0f;
-    [Range(0,1)]
-    [SerializeField] float xScale = 1f;
-    [Range(0, 1)]
-    [SerializeField] float zScale = 1f;
-    int SampleLength;
-    bool spawned;
-
-
-
-    private void Start()
+    public class CubeSpawner : MonoBehaviour
     {
-        SampleLength = AudioPeer.Instance.Samples.Length;
-        SpawnCube();
-    }
+        [SerializeField] private GameObject cube;
+        private GameObject[] cubes;
 
-    private void Update()
-    {
-        if (!spawned) return;
+        [Header("Settings")]
+        [SerializeField] float spawnRadius = 10f;
+        [Range(0f, 100f)]
+        [SerializeField] float sizePower = 20f;
 
-        for(int i = 0; i < SampleLength; i++)
+        [Space(2)]
+        [Header("Scales")]
+        [Range(0, 2)]
+        [SerializeField] float minYScale = 1.0f;
+        [Range(0, 1)]
+        [SerializeField] float xScale = 1f;
+        [Range(0, 1)]
+        [SerializeField] float zScale = 1f;
+        int SampleLength;
+        bool spawned;
+
+
+
+        private void Start()
         {
-           cubes[i].transform.localScale = new Vector3(xScale, (AudioPeer.Instance.Samples[i] * sizePower) + minYScale, zScale);
-            //cubes[i].transform.localScale = new Vector3(xScale,  zScale, (AudioPeer.Instance.Samples[i] * sizePower) + minYScale);
+            SampleLength = AudioPeer.Instance.Samples.Length;
+            SpawnCube();
         }
-    }
 
-    Vector3 dir;
-    Vector3 pos;
-
-    void SpawnCube()
-    {
-        cubes = new GameObject[SampleLength];
-        
-        for(int i = 0; i < SampleLength; i++)
+        private void Update()
         {
-            float angle = i * 360f / SampleLength;
-            dir = Quaternion.Euler(0,angle,0)*transform.forward;
-            pos = transform.position + dir * spawnRadius;
+            if (!spawned) return;
 
-            GameObject newCube = Instantiate(cube, pos, Quaternion.identity, transform);
-            cube.name = $"Cube {i}";
-            cubes[i] = newCube;
+            for (int i = 0; i < SampleLength; i++)
+            {
+                cubes[i].transform.localScale = new Vector3(xScale, (AudioPeer.Instance.Samples[i] * sizePower) + minYScale, zScale);
+            }
         }
-        spawned = true;
+
+        Vector3 dir;
+        Vector3 pos;
+
+        void SpawnCube()
+        {
+            cubes = new GameObject[SampleLength];
+
+            for (int i = 0; i < SampleLength; i++)
+            {
+                float angle = i * 360f / SampleLength;
+                dir = Quaternion.Euler(0, angle, 0) * transform.forward;
+                pos = transform.position + dir * spawnRadius;
+
+                GameObject newCube = Instantiate(cube, pos, Quaternion.identity, transform);
+                cube.name = $"Cube {i}";
+                cubes[i] = newCube;
+            }
+            spawned = true;
+        }
     }
 }
